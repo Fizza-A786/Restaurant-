a3815987d04f43634281abb5538d491b89e4d057
Fizza-A786 <atesh7865@gmail.com>
2026-05-26 13:25:56 +0500

    remove component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-
-import NavbarTop from "./Common/NavbarTop"
 import Navbar from "./Common/Navbar"
-import Home from "./Pages/Home"
-import Footer from "./Common/Footer"
 
 const App = () => {
   return (
     <>
-      <NavbarTop />
-      <Navbar />
-      <Home />
-      <Footer />
+    
+   <Navbar/>
+
     </>
   )
 }
